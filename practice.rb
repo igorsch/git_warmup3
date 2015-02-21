e6ac4fc9a83b7b8121db809061b31daa85be6701
Igor Shcherbak <igorski_is@yahoo.com>
2015-02-21 12:31:42 -0700

    made some changes to practice.rb

--- practice.rb
+++ practice.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-num = 20
+num = 9
 
 num_bot = proc { |n| "#{n} bottle#{n == 1 ? '' : 's'}"}
 
 while num > 0
-  puts num_bot[num]
+  puts "We have #{num_bot[num]}"
   num -= 1
 end
