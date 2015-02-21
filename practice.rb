1b04b060100c9e4ec136d25407f73386f3b2923c
Igor Shcherbak <igorski_is@yahoo.com>
2015-02-21 15:29:12 -0700

    Improved practice.rb [#234567]

--- practice.rb
+++ practice.rb
@@ -2,10 +2,17 @@
 
 num = 9
 
-num_bot = proc { |n| "#{n} bottle#{n == 1 ? '' : 's'}"}
+def num_bot(n)
+  if n > 1
+    "#{n} bottles"
+  elsif n == 1
+    "#{n} bottle"
+  else
+    'no bottles'
+  end
+end
 
 while num > 0
-  puts "We have #{num_bot[num]}"
-  puts "No, we don't. We have #{num_bot[num]} left."
+  puts "We have #{num_bot(num)}, and we drink one. Now we have #{num_bot(num - 1)}"
   num -= 1
 end
